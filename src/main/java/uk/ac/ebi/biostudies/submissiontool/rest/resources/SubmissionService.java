23a3cc75e2e68f1986ca98575bba8ff076e68d62
olgamelnichuk <olkin@ebi.ac.uk>
2017-04-18 12:00:01 +0100

    code cleanup

--- src/main/java/uk/ac/ebi/biostudies/submissiontool/rest/resources/SubmissionService.java
+++ src/main/java/uk/ac/ebi/biostudies/submissiontool/rest/resources/SubmissionService.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 import rx.Observable;
 import rx.exceptions.Exceptions;
 import uk.ac.ebi.biostudies.submissiontool.bsclient.BioStudiesClient;
-import uk.ac.ebi.biostudies.submissiontool.bsclient.BioStudiesClientException;
 import uk.ac.ebi.biostudies.submissiontool.europepmc.EuropePMCClient;
 import uk.ac.ebi.biostudies.submissiontool.rest.data.ModifiedSubmission;
 import uk.ac.ebi.biostudies.submissiontool.rest.data.PageTabUtils;
@@ -93,26 +92,6 @@ public class SubmissionService {
                     }
                 });
     }
-/*
-    public ModifiedSubmission getSubmission(final String accno, boolean origin, UserSession session)
-            throws BioStudiesClientException, IOException {
-        logger.debug("getSubmission(session={}, accnoAttr={}, origin={})", session, accno, origin);
-        if (!origin) {
-            String resp = bsclient.getModifiedSubmission(accno, session.id());
-            if (!resp.isEmpty()) {
-                return ModifiedSubmission.parse(resp);
-            }
-        }
-        return ModifiedSubmission.wrap(bsclient.getSubmission(accno, session.id()));
-    }
-*/
-
-   /* public ModifiedSubmission createSubmission(String subm, UserSession session) throws IOException, BioStudiesClientException {
-        logger.debug("createSubmission(session={})", session);
-        ModifiedSubmission modified = ModifiedSubmission.wrap(subm);
-        saveSubmission(modified, session);
-        return modified;
-    }*/
 
     public Observable<String> createSubmissionRx(String subm, UserSession session) throws IOException {
         logger.debug("createSubmission(session={})", session);
@@ -125,30 +104,6 @@ public class SubmissionService {
     public Observable<String> saveSubmissionRx(String subm, String accno, UserSession session) {
         return bsclient.saveModifiedSubmissionRx(subm, accno, session.id());
     }
-
-/*
-    public void saveSubmission(String subm, UserSession session) throws IOException, BioStudiesClientException {
-        this.saveSubmission(ModifiedSubmission.parse(subm), session);
-    }
-
-    private void saveSubmission(ModifiedSubmission subm, UserSession session) throws IOException, BioStudiesClientException {
-        logger.debug("saveSubmission(session={}, obj={})", session, subm);
-        bsclient.saveModifiedSubmission(subm.update().json().toString(), subm.getAccno(), session.id());
-    }
-*/
-
-   /* public String editSubmission(String accno, UserSession session)
-            throws BioStudiesClientException, IOException {
-        logger.debug("editSubmission(session={}, accnoAttr={})", session, accno);
-        String sbm = bsclient.getModifiedSubmission(accno, session.id());
-        if (sbm.isEmpty()) {
-            logger.debug("no temporary copy; creating one...");
-            ModifiedSubmission modified = getSubmission(accno, true, session);
-            saveSubmission(modified, session);
-            return modified.json().toString();
-        }
-        return sbm;
-    }*/
 
     public Observable<String> submitModifiedRx(String subm, UserSession session) throws IOException {
         logger.debug("submitSubmission(session={}, obj={})", session, subm);
@@ -169,37 +124,14 @@ public class SubmissionService {
                 });
     }
 
-    /*public String submitModified(String subm, UserSession session) throws IOException, BioStudiesClientException {
-        logger.debug("submitSubmission(session={}, obj={})", session, subm);
-        ModifiedSubmission modified = ModifiedSubmission.parse(subm);
-        String result = submit(modified.isNew(), modified.getData(), session);
-
-        //TODO: better way to read BioStudies API response
-        JsonNode resultNode = objectMapper().readTree(result);
-        String status = resultNode.get("status").asText();
-        if (status.equals("OK")) {
-            deleteSubmission(modified.getAccno(), session);
-        }
-        return result;
-    }*/
-
     public Observable<String> submitPlainRx(boolean create, String subm, UserSession session) throws IOException {
         JsonNode node = objectMapper().readTree(subm);
         return submitRx(create, node, session);
     }
 
-   /* public String submitPlain(boolean create, String subm, UserSession session) throws IOException, BioStudiesClientException {
-        JsonNode node = objectMapper().readTree(subm);
-        return submit(create, node, session);
-    }*/
-
     private Observable<String> submitRx(boolean create, JsonNode subm, UserSession session) {
         return create ? submitNewRx(subm, session) : submitExistedRx(subm, session);
     }
-
-    /*private String submit(boolean create, JsonNode subm, UserSession session) throws IOException, BioStudiesClientException {
-        return create ? submitNew(subm, session) : submitExisted(subm, session);
-    }*/
 
     private Observable<String> submitNewRx(JsonNode subm, UserSession session) {
         return accnoTemplateRx(subm, session)
@@ -224,51 +156,6 @@ public class SubmissionService {
         submissions.set("submissions", array);
         return bsclient.submitUpdatedRx(submissions.toString(), session.id());
     }
-
-/*
-    private String submitNew(JsonNode subm, UserSession session) throws IOException, BioStudiesClientException {
-        ObjectNode copy = subm.deepCopy();
-        copy.put("accno", accnoTemplate(copy, session));
-
-        ArrayNode array = JsonNodeFactory.instance.arrayNode();
-        array.add(copy);
-
-        ObjectNode submissions = JsonNodeFactory.instance.objectNode();
-        submissions.set("submissions", array);
-
-        return bsclient.submitNew(submissions.toString(), session.id());
-    }*/
-
-   /* private String submitExisted(JsonNode subm, UserSession session) throws IOException, BioStudiesClientException {
-        ArrayNode array = JsonNodeFactory.instance.arrayNode();
-        array.add(subm);
-
-        ObjectNode submissions = JsonNodeFactory.instance.objectNode();
-        submissions.set("submissions", array);
-        return bsclient.submitUpdated(submissions.toString(), session.id());
-    }
-*/
-    /*private String accnoTemplate(JsonNode subm, UserSession session) throws IOException, BioStudiesClientException {
-        final String defaultTmpl = "!{S-BSST}";
-
-        List<String> accessions = PageTabUtils.attachToAttr(subm);
-        if (accessions.size() != 1) {
-            return defaultTmpl;
-        }
-
-        String accno = accessions.get(0);
-        if (accno == null || accno.isEmpty()) {
-            return defaultTmpl;
-        }
-        String accnoTmpl = projectAccnoTemplate(accno, session);
-        return accnoTmpl == null || accno.isEmpty() ? defaultTmpl : accnoTmpl;
-    }
-
-
-    private String projectAccnoTemplate(String accno, UserSession session) throws IOException, BioStudiesClientException {
-        ModifiedSubmission proj = getSubmission(accno, true, session);
-        return PageTabUtils.accnoTemplateAttr(proj.getData());
-    }*/
 
     private Observable<String> accnoTemplateRx(JsonNode subm, UserSession session) {
         final Observable<String> defaultTmpl = Observable.just("!{S-BSST}");
@@ -298,25 +185,6 @@ public class SubmissionService {
                 });
     }
 
- /*   public boolean deleteSubmission(String acc, UserSession session)
-            throws BioStudiesClientException, IOException {
-        logger.debug("deleteSubmission(session={}, acc={})", session, acc);
-        String sbm = bsclient.getModifiedSubmission(acc, session.id());
-        if (!sbm.isEmpty()) {
-            bsclient.deleteModifiedSubmission(acc, session.id());
-            return true;
-        }
-        sbm = bsclient.getSubmission(acc, session.id());
-        //TODO do it better way
-        if (sbm.isEmpty()) {
-            return true;
-        }
-        String resp = bsclient.deleteSubmission(acc, session.id());
-        JsonNode json = objectMapper().readTree(resp);
-        String level = json.get("level").asText();
-        return level.equalsIgnoreCase("success");
-    }*/
-
     public Observable<Boolean> deleteSubmissionRx(String accno, UserSession session) {
         logger.debug("deleteSubmission(session={}, acc={})", session, accno);
         return getModifiedRx(accno, session)
@@ -344,45 +212,6 @@ public class SubmissionService {
                 .map(resp -> true);
     }
 
-    /*
-    public String getSubmittedSubmissions(int offset, int limit, Map<String, String> paramMap, UserSession session)
-            throws BioStudiesClientException, IOException {
-        String sessId = session.id();
-        return bsclient.getSubmissions(sessId, offset, limit, paramMap);
-    }
-      public String getModifiedSubmissions(int offset, int limit, Map<String, String> paramMap, UserSession session)
-            throws BioStudiesClientException, IOException {
-        SubmFilterParams params = SubmFilterParams.fromMap(paramMap);
-        String sessId = session.id();
-
-        JsonNode json = objectMapper().readTree(bsclient.getModifiedSubmissions(sessId));
-        List<SubmissionListItem> submList = new ArrayList<>();
-
-        if (json.isArray()) {
-            json.forEach((JsonNode node) -> {
-                try {
-                    submList.add(SubmissionListItem.from(ModifiedSubmission.convert(node)));
-                } catch (JsonProcessingException e) {
-                    logger.error("Converting modified submissions error", e);
-                }
-            });
-        }
-
-        submList.sort(SubmissionListItem.sortByMTime());
-        List<SubmissionListItem> filtered =
-                submList
-                        .stream()
-                        .filter(params.asPredicate())
-                        .skip(offset)
-                        .limit(limit)
-                        .collect(Collectors.toList());
-
-        ObjectNode obj = JsonNodeFactory.instance.objectNode();
-        obj.set("submissions", objectMapper().valueToTree(filtered));
-        return obj.toString();
-    }
-
-*/
     public Observable<String> getSubmittedSubmissionsRx(int offset, int limit, Map<String, String> paramMap, UserSession session) {
         return bsclient.getSubmissionsRx(session.id(), offset, limit, paramMap);
         // todo check status property ?
@@ -428,12 +257,6 @@ public class SubmissionService {
                 });
     }
 
-/*
-    public String getProjects(UserSession session) throws BioStudiesClientException, IOException {
-        return bsclient.getProjects(session.id());
-    }
-*/
-
     public Observable<String> getProjectsRx(UserSession session) {
         return bsclient.getProjectsRx(session.id());
     }
@@ -443,30 +266,10 @@ public class SubmissionService {
         return bsclient.getFilesDirRx(path, depth, showArchive, session.id());
     }
 
-   /* public String getFilesDir(String path, int depth, boolean showArchive, UserSession session) throws BioStudiesClientException, IOException {
-        logger.debug("getFilesDir(session={}, path={}, depth={}, showArchive={})", session, path, depth, showArchive);
-        return bsclient.getFilesDir(path, depth, showArchive, session.id());
-    }*/
-
     public Observable<String> deleteFileRx(String path, UserSession session) {
         logger.debug("deleteFile(session={}, path={})", session, path);
         return bsclient.deleteFileRx(path, session.id());
     }
-
-/*
-    public String deleteFile(String path, UserSession session)
-            throws BioStudiesClientException, IOException {
-        logger.debug("deleteFile(session={}, path={})", session, path);
-        return bsclient.deleteFile(path, session.id());
-    }
-*/
-
-   /* public String signOut(UserSession session) throws BioStudiesClientException, IOException {
-        logger.debug("signOut(session={})", session);
-        ObjectNode obj = JsonNodeFactory.instance.objectNode();
-        obj.put("sessid", session.id());
-        return bsclient.signOut(obj.toString(), session.id());
-    }*/
 
     public Observable<String> signOutRx(UserSession session)  {
         logger.debug("signOut(session={})", session);
@@ -474,20 +277,6 @@ public class SubmissionService {
         obj.put("sessid", session.id());
         return bsclient.signOutRx(obj.toString(), session.id());
     }
-
-   /* public String signUp(SignUpParams params) throws BioStudiesClientException, IOException {
-        logger.debug("signUp(obj={})", params);
-        ObjectNode json = JsonNodeFactory.instance.objectNode();
-        json.put("username", params.getUsername());
-        json.put("password", params.getPassword());
-        json.put("email", params.getEmail());
-        json.put("recaptcha2-response", params.getCaptcha());
-        json.put("activationURL", params.getPath());
-        ArrayNode aux = JsonNodeFactory.instance.arrayNode();
-        aux.add("orcid:" + (params.hasOrcid() ? params.getOrcid() : ""));
-        json.set("aux", aux);
-        return bsclient.signUp(json.toString());
-    }*/
 
     public Observable<String> signUpRx(SignUpParams params) {
         logger.debug("signUp(obj={})", params);
@@ -503,15 +292,6 @@ public class SubmissionService {
         return bsclient.signUpRx(json.toString());
     }
 
-    /*public String passwordResetRequest(EmailPathCaptchaParams params) throws BioStudiesClientException, IOException {
-        logger.debug("passwordResetRequest(obj={})", params);
-        ObjectNode json = JsonNodeFactory.instance.objectNode();
-        json.put("email", params.getEmail());
-        json.put("recaptcha2-response", params.getCaptcha());
-        json.put("resetURL", params.getPath());
-        return bsclient.passwordResetRequest(json.toString());
-    }
-*/
     public Observable<String> passwordResetRequestRx(EmailPathCaptchaParams params) {
         logger.debug("passwordResetRequest(obj={})", params);
         ObjectNode json = JsonNodeFactory.instance.objectNode();
@@ -520,16 +300,6 @@ public class SubmissionService {
         json.put("resetURL", params.getPath());
         return bsclient.passwordResetRequestRx(json.toString());
     }
-
-  /*  public String passwordReset(KeyPasswordCaptchaParams params) throws IOException, BioStudiesClientException {
-        logger.debug("passwordReset(obj={})", params);
-        ObjectNode json = JsonNodeFactory.instance.objectNode();
-        json.put("key", params.getKey());
-        json.put("password", params.getPassword());
-        json.put("recaptcha2-response", params.getCaptcha());
-        return bsclient.passwordReset(json.toString());
-    }*/
-
 
     public Observable<String> passwordResetRx(KeyPasswordCaptchaParams params) {
         logger.debug("passwordReset(obj={})", params);
@@ -540,15 +310,6 @@ public class SubmissionService {
         return bsclient.passwordResetRx(json.toString());
     }
 
-   /* public String resendActivationLink(EmailPathCaptchaParams params) throws IOException, BioStudiesClientException {
-        logger.debug("resendActivationLink(params={})", params);
-        ObjectNode json = JsonNodeFactory.instance.objectNode();
-        json.put("email", params.getEmail());
-        json.put("recaptcha2-response", params.getCaptcha());
-        json.put("activationURL", params.getPath());
-        return bsclient.resendActivationLink(json.toString());
-    }*/
-
     public Observable<String> resendActivationLinkRx(EmailPathCaptchaParams params) {
         logger.debug("resendActivationLink(params={})", params);
         ObjectNode json = JsonNodeFactory.instance.objectNode();
@@ -558,47 +319,10 @@ public class SubmissionService {
         return bsclient.resendActivationLinkRx(json.toString());
     }
 
-  /*  public String activate(String key) throws IOException, BioStudiesClientException {
-        logger.debug("activate(key={})", key);
-        return bsclient.activate(key);
-    }*/
-
-
     public Observable<String> activateRx(String key) {
         logger.debug("activate(key={})", key);
         return bsclient.activateRx(key);
     }
-/*
-
-    public String pubMedSearch(String id) {
-        logger.debug("pubMedSearch(ID={})", id);
-        ObjectNode result = JsonNodeFactory.instance.objectNode();
-
-        try {
-            JsonNode resp = objectMapper().readTree(europePmc.pubMedSearch(id));
-
-            int hitCount = resp.path("hitCount").asInt();
-            ObjectNode data = JsonNodeFactory.instance.objectNode();
-
-            if (hitCount >= 1) {
-                final JsonNode publ = resp.path("resultList").path("result").get(0);
-                europePmcAttributes.forEach((key, value) -> {
-                    if (publ.has(key)) {
-                        data.put(value, publ.get(key).asText(""));
-                    }
-                });
-            }
-            result.put("status", "OK");
-            result.set("data", data);
-        } catch (IOException e) {
-            logger.warn("EuropePMC call for ID={} failed: {}", id, e);
-            result.put("status", "FAIL");
-            result.set("data", JsonNodeFactory.instance.objectNode());
-        }
-        return result.toString();
-    }
-*/
-
 
     public Observable<String> pubMedSearchRx(String id) {
         logger.debug("pubMedSearch(ID={})", id);
