9ae35831057770e2bb5b3e8abae8586f13531978
olgamelnichuk <olkin@ebi.ac.uk>
2018-02-12 16:30:47 +0000

    bugfix: return empty object instead of empty string; angular http response parser complains about getting empty string as json response

--- src/main/java/uk/ac/ebi/biostudies/submissiontool/bsclient/BioStudiesRestClient.java
+++ src/main/java/uk/ac/ebi/biostudies/submissiontool/bsclient/BioStudiesRestClient.java
@@ -518,6 +518,10 @@ public class BioStudiesRestClient implements BioStudiesClient {
         if (!getStatus(body).equalsIgnoreCase("ok")) {
             throw new BioStudiesRxClientException(422, mediaType == null ? MediaType.TEXT_PLAIN : mediaType.getType(), body);
         }
+
+        if (body.isEmpty()) {
+            body = "{}";
+        }
         return body;
     }
 
